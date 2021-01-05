093d7a449533436f70473e1f82fb6b31bbe0aeb9
pmmccorkell <74677486+pmmccorkell@users.noreply.github.com>
2021-01-05 12:56:34 -0500

    asdf
    asdf

---
+++ callback.py
@@ -0,0 +1,9 @@
+def MESSAGE_CALLBACK(client,userdata,message):
+	print()
+	print("mqtt rx:")
+	print(message.topic)
+	print(message.qos)
+	print(message.payload)
+	print(message.payload.decode())
+	print(message.retain)
+	print(client)
